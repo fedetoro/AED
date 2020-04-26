2c85addcd010cb2c228f58d78cf6bb70e9413595
Federico Nicolas Gonzalez Toro <63463118+fedetoro@users.noreply.github.com>
2020-04-26 13:34:51 -0300

    Add files via upload

---
+++ 01-Adición/Adición.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+
+int main(){
+	int a, b;
+	std::cout << "Prueba de Adicion:\n" ;
+	std::cout << "Ingrese el primer numero entero\n";
+	std::cin >> a;
+	std::cout << "Ingrese el segundo numero entero\n";	
+	std::cin >> b;
+	std::cout << "La suma de ambos numeros es: \n";
+	std::cout << a+b;
+}
