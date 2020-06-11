31a97c7c659a7f47a4b8ba9520fe5d5b3cf66680
Federico Nicolas Gonzalez Toro <63463118+fedetoro@users.noreply.github.com>
2020-06-11 17:38:30 -0300

    Add files via upload

---
+++ 03-Bisiesto/IsBisiesto.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+#include <cassert>
+using namespace std;
+
+//Declaracion de datos
+bool IsBisiesto (unsigned a);
+
+//funcion main
+int main (){
+    assert (not IsBisiesto(1582));
+    assert (not IsBisiesto(1749));
+    assert (IsBisiesto(1600));
+    assert (IsBisiesto(2020));
+}
+
+//Declaracion de funciones
+bool IsBisiesto(unsigned a)
+{   return
+    a > 1582 and (a%4 == 0) and ((a%100 != 0) or (a%400 == 0));
+}
